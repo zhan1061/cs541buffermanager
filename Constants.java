86cbd03e0ba1d49d5298129760b54380a15f5c29
vinayganesh <vinayganesh@47be73ae-190b-11df-8ab4-19918e372ae5>
2010-02-14 15:01:14 +0000

    removed old stuff

--- Constants.java
+++
@@ -1,7 +0,0 @@
-package bufmgr;
-
-public class Constants{
-	public static final int NO_FRAME_FOUND = -1;
-	public static final int NO_PAGE = -1;
-	public static final int PAGE_HASHTABLE_SIZE = 23;
-}
