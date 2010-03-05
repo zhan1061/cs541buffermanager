255c5bfb8571708fff5aeb3332257a239c897d1a
vinayganesh <vinayganesh@47be73ae-190b-11df-8ab4-19918e372ae5>
2010-03-05 15:40:37 +0000

    updated indexscan and projection

--- Project3/src/relop/IndexScan.java
+++ Project3/src/relop/IndexScan.java
@@ -1,7 +1,9 @@
 package relop;
 
+import global.RID;
 import global.SearchKey;
 import heap.HeapFile;
+import index.BucketScan;
 import index.HashIndex;
 
 /**
@@ -9,11 +11,19 @@ import index.HashIndex;
  */
 public class IndexScan extends Iterator {
 
+	private BucketScan bucketScan;
+	private HashIndex hashIndex;
+	private HeapFile file;
+	
+	
   /**
    * Constructs an index scan, given the hash index and schema.
    */
   public IndexScan(Schema schema, HashIndex index, HeapFile file) {
-    throw new UnsupportedOperationException("Not implemented");
+	  this.schema = schema;
+	  this.hashIndex = index;
+	  this.file = file;
+	  this.bucketScan = this.hashIndex.openScan();
   }
 
   /**
@@ -21,35 +31,41 @@ public class IndexScan extends Iterator {
    * child iterators, and increases the indent depth along the way.
    */
   public void explain(int depth) {
-    throw new UnsupportedOperationException("Not implemented");
+	  System.out.println("Index Scan");
   }
 
   /**
    * Restarts the iterator, i.e. as if it were just constructed.
    */
   public void restart() {
-    throw new UnsupportedOperationException("Not implemented");
+	  if(bucketScan != null ){
+		  bucketScan.close();
+	  }
+	  bucketScan = hashIndex.openScan();
   }
 
   /**
    * Returns true if the iterator is open; false otherwise.
    */
   public boolean isOpen() {
-    throw new UnsupportedOperationException("Not implemented");
+	  return bucketScan != null;
   }
 
   /**
    * Closes the iterator, releasing any resources (i.e. pinned pages).
    */
   public void close() {
-    throw new UnsupportedOperationException("Not implemented");
+	  if(bucketScan !=null){
+		  bucketScan.close();
+		  bucketScan = null;
+	  }
   }
 
   /**
    * Returns true if there are more tuples, false otherwise.
    */
   public boolean hasNext() {
-    throw new UnsupportedOperationException("Not implemented");
+	  return bucketScan.hasNext();
   }
 
   /**
@@ -58,14 +74,20 @@ public class IndexScan extends Iterator {
    * @throws IllegalStateException if no more tuples
    */
   public Tuple getNext() {
-    throw new UnsupportedOperationException("Not implemented");
+	  RID rid = bucketScan.getNext();
+	  byte [] data = file.selectRecord(rid);
+	  if(data != null){
+		  return new Tuple (schema, data);
+	  }else{
+		  throw new IllegalStateException("No more tuples left");
+	  }
   }
 
   /**
    * Gets the key of the last tuple returned.
    */
   public SearchKey getLastKey() {
-    throw new UnsupportedOperationException("Not implemented");
+	  return bucketScan.getLastKey();
   }
 
   /**
@@ -73,7 +95,7 @@ public class IndexScan extends Iterator {
    * number of buckets if none.
    */
   public int getNextHash() {
-    throw new UnsupportedOperationException("Not implemented");
+	  return bucketScan.getNextHash();
   }
 
 } // public class IndexScan extends Iterator
--- Project3/src/relop/Projection.java
+++ Project3/src/relop/Projection.java
@@ -5,12 +5,29 @@ package relop;
  * relational algebra, this operator does NOT eliminate duplicate tuples.
  */
 public class Projection extends Iterator {
+	
+	Integer[] fields;
+	Iterator iter;
+	int[] mapping;
 
   /**
-   * Constructs a projection, given the underlying iterator and field numbers.
+   * Constructs a projection, given the underlying iator and field numbers.
    */
   public Projection(Iterator iter, Integer... fields) {
-    throw new UnsupportedOperationException("Not implemented");
+	  this.iter = iter;
+	  this.fields = fields;
+
+	  schema = new Schema(fields.length);
+	  mapping= new int[fields.length];
+
+	  for(int i=0;i< fields.length; i++ ){
+		  String fieldName = iter.schema.fieldName(fields[i]);
+		  int fieldLength = iter.schema.fieldLength(fields[i]);
+		  int fieldType = iter.schema.fieldType(fields[i]);
+
+		  schema.initField(i, fieldType,fieldLength, fieldName);
+		  mapping[i] = fields[i];
+	  }
   }
 
   /**
@@ -18,35 +35,35 @@ public class Projection extends Iterator {
    * child iterators, and increases the indent depth along the way.
    */
   public void explain(int depth) {
-    throw new UnsupportedOperationException("Not implemented");
+	  System.out.println("Projection Op...");
   }
 
   /**
    * Restarts the iterator, i.e. as if it were just constructed.
    */
   public void restart() {
-    throw new UnsupportedOperationException("Not implemented");
+	  iter.restart();
   }
 
   /**
    * Returns true if the iterator is open; false otherwise.
    */
   public boolean isOpen() {
-    throw new UnsupportedOperationException("Not implemented");
+	  return iter.isOpen();
   }
 
   /**
    * Closes the iterator, releasing any resources (i.e. pinned pages).
    */
   public void close() {
-    throw new UnsupportedOperationException("Not implemented");
+	    iter.close();
   }
 
   /**
    * Returns true if there are more tuples, false otherwise.
    */
   public boolean hasNext() {
-    throw new UnsupportedOperationException("Not implemented");
+	   return iter.hasNext();
   }
 
   /**
@@ -55,7 +72,15 @@ public class Projection extends Iterator {
    * @throws IllegalStateException if no more tuples
    */
   public Tuple getNext() {
-    throw new UnsupportedOperationException("Not implemented");
+	  Tuple projTuple = new Tuple(schema);
+	  int numOfFields = schema.getCount();
+	  Tuple origTuple = iter.getNext();
+
+	  for(int i=0;i<numOfFields;i++){
+		  projTuple.setField(i, origTuple.getField(mapping[i]));
+	  }
+
+      return projTuple;
   }
 
 } // public class Projection extends Iterator
